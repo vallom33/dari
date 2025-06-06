20580d93e25063ca0edaf02543c087bf5e69d48c
vallom33 <mdvallahmedoomar@gmail.com>
2025-06-06 13:01:23 +0000

    navbar

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,10 +1,14 @@
-import { type ApplicationConfig, provideZoneChangeDetection } from "@angular/core"
+import { type ApplicationConfig, provideZoneChangeDetection, isDevMode } from "@angular/core"
 import { provideRouter } from "@angular/router"
 import { provideHttpClient } from "@angular/common/http"
 
 import { routes } from "./app.routes";
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideServiceWorker } from '@angular/service-worker'
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(), provideAnimationsAsync()],
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(), provideAnimationsAsync(), provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })],
 }
